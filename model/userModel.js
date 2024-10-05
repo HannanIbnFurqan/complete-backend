1aabee2707214a60ff534d9f317c706ecdf452a0
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-05 12:55:09 +0530

    complete userSchema

---
+++ model/userModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    rols:{
+        type:Number,
+        default:0
+    },
+    cart:{
+        type:Array,
+        default:[]
+    }
+    
+},{timestamps:true})
+
+const User = new mongoose.model('user',userSchema);
+export default User
