8f2995f314688f200c60b60c1bec0dbb0c40699a
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-05 12:53:56 +0530

    backend connection successfully

---
+++ connectDB/connectDB.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose"
+import dotenv from 'dotenv'
+dotenv.config()
+const connectDB = async () =>{
+  try {
+    const dbURL = process.env.DB_URI
+    await mongoose.connect(dbURL)
+    console.log('connection successfully')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default connectDB
