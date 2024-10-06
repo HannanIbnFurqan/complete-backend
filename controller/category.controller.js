7d0ad9ebd8b62984b873c268d5b6b3fbd7dc1701
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-07 00:16:46 +0530

    add createCategory functionality

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -1,6 +1,33 @@
+import Category from "../model/categoryModel.js"
+
 const categoryController = {
     getCategory: async (req,res)=>{
-        res.json('testing')
+        try {
+            const categories = await Category.find()
+            res.json(categories)
+        } catch (error) {
+            res.status(500).json({err: error.messsage})
+        }
+       
+    },
+    createCategory:async (req,res)=>{
+       try {
+        // get category by name
+        const {name} = req.body
+        // find by name
+        const category = await Category.findOne({name})
+        
+        if(category) return res.status(400).json({message: "category already exist"})
+       
+        const newCategory = new Category({name})
+        
+        await newCategory.save()
+
+        res.json({messsage: "category is created"})
+
+       } catch (error) {
+        res.status(500).json({err: error.message})
+       }
     }
 }
 
