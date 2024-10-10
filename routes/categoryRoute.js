696c0b1049e0ce2a3c25b03dabcf4af51545cf00
HannanIbnFurqan <mohdhannan7913@gmail.com>
2024-10-10 11:46:33 +0530

    add delete route

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,10 +2,16 @@ import express from "express";
 import categoryController from "../controller/category.controller.js";
 import auth from "../middleware/auth.js";
 import authAdmin from "../middleware/authAdmin.js";
-const router = express.Router()
 
+const router = express.Router();
+
+// Route to get all categories and create a new one
 router.route('/category')
-.get(categoryController.getCategory)
-.post(auth, authAdmin, categoryController.createCategory)
+  .get(categoryController.getCategory)
+  .post(auth, authAdmin, categoryController.createCategory);
 
-export default router
+// Route to delete a category by ID
+router.route('/category/:id')
+  .delete(auth, authAdmin, categoryController.deleteCategory);
+
+export default router;
